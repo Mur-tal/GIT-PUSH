e3338919305b7ba55b54da98da3f0b8f60af88d4
mur-tal <muri.gbadamosi@gmail.com>
2023-06-05 20:42:55 +0100

    file two

---
+++ git_add_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# adds all files in the working directory to the staging area
+git add .
+
+# saves files in the staging area to the local repository
+read -p "Enter your commit message: " commit # input from user
+git commit -m "$commit"
+
+# this line sends your changes to the remote repository
+git push
